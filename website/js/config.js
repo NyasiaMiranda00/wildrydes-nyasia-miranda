89632226315d2f0141b608c431e7e84572fa86b1
NyasiaMiranda00 <raven10ny@gmail.com>
2019-11-18 13:58:41 -0500

    config.js was changed

---
+++ website/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_z6lATyESP',
+        userPoolClientId: '4jtt1vl088sldal4lndgjr6b8r',
+        region: 'us-east-1:482949084875'
+    },
+    api: {
+        invokeUrl: 'https://rc7nyt4tql.execute-api.us-east-1.amazonaws.com/prod',
+    }
+};
